6815a5dc2fb0798d119ec4c3776d5a25d1808b59
shreya <limsumi141@gmail.com>
2020-08-28 20:56:39 +0545

    git init:test java program

---
+++ src/com/egjava/demojava/Demo.java
@@ -0,0 +1,8 @@
+package com.egjava.demojava;
+public class Demo {
+    public static void main(String[] args) {
+        int integerOne=20;
+        int integerTwo=integerOne+1;
+        System.out.println(integerTwo);
+    }
+}
---
+++ src/com/java/helloworld/HelloWorld.java
@@ -0,0 +1,46 @@
+package com.java.helloworld;
+public class HelloWorld {
+    public static void main(String[] args) {
+        byte bigByte = 127;
+        short bigShort = 32767;
+        int bigInt = 2100000000;
+        float bigFloat = 4.14f;
+        long bigLong = 13450000000L;
+        double bigDouble = 2.23451234134;
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        boolean trueOrFalse = true;
+        char randomChar = 65;
+        char anotherChar = 'A';
+        char escapedChars='\\';
+        String randomString="i am a random string";
+        String anotherString="stuff";
+        System.out.println(randomChar);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
